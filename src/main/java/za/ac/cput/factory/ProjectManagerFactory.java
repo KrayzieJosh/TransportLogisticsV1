9578a17b4bee7c7b81701bc41f026a650ec5fac4
MZainGamildien <102467824+MZainGamildien@users.noreply.github.com>
2023-06-11 20:54:29 +0200

    Project Class and tests finished

--- src/main/java/za/ac/cput/factory/ProjectManagerFactory.java
+++ src/main/java/za/ac/cput/factory/ProjectManagerFactory.java
@@ -2,13 +2,14 @@ package za.ac.cput.factory;
 
 import za.ac.cput.domain.Project;
 import za.ac.cput.domain.ProjectManager;
+import za.ac.cput.domain.SiteManager;
 import za.ac.cput.util.Helper;
 
 import java.util.List;
 
 public class ProjectManagerFactory {
     public static ProjectManager createProjectManager(String userId, String userPosition,
-                                                      List<Project> userProjects) {
+                                                      List<Project> userProjects, List<SiteManager>siteManagers) {
         userId = Helper.generateID();
         if (Helper.isNullOrEmpty(userId) || Helper.isNullOrEmpty(userPosition)
                 || Helper.isNullOrEmpty(String.valueOf(userProjects))) {
@@ -19,7 +20,7 @@ public class ProjectManagerFactory {
         return new ProjectManager.ProjectManagerBuilder()
                 .setUserId(userId)
                 .setUserPosition(userPosition)
-                .setUserProjects(userProjects)
+                .setUserProjects(userProjects).setSiteManager(siteManagers)
                 .build();
     }
 
