83416d29e4b9b2e232a85c9d8466a61376b89c06
Joshua Daniel Jacobs <114062249+KrayzieJosh@users.noreply.github.com>
2023-09-04 19:24:44 +0200

    added application.properties & mysql dependency

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,22 +1,10 @@
 package za.ac.cput;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import za.ac.cput.domain.Project;
-import za.ac.cput.domain.ProjectManager;
-import za.ac.cput.factory.ProjectFactory;
-import za.ac.cput.factory.ProjectManagerFactory;
-import za.ac.cput.service.ProjectManagerService;
-import za.ac.cput.service.ProjectService;
-import za.ac.cput.util.Helper;
 
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @SpringBootApplication
